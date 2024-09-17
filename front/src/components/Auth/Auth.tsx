8c4d182e3a90382d04438b23e6c7337246f9240f
pauldekarin <bluerayy123@gmail.com>
2024-09-18 00:42:21 +0700

    update

--- front/src/components/Auth/Auth.tsx
+++ front/src/components/Auth/Auth.tsx
@@ -1,8 +1,9 @@
 import React, { SyntheticEvent } from 'react'
-
+import styles from './Auth.module.css'
 interface AuthFormState{
     login: string;
     password: string;
+    authorized: boolean;
 }
 
 export class AuthForm extends React.Component<{}, AuthFormState>{
@@ -11,7 +12,8 @@ export class AuthForm extends React.Component<{}, AuthFormState>{
     
         this.state = {
             login: '',
-            password: ''
+            password: '',
+            authorized: false
         };
 
         this.handleChange = this.handleChange.bind(this);
@@ -20,7 +22,7 @@ export class AuthForm extends React.Component<{}, AuthFormState>{
 
     async handleChange(event: React.ChangeEvent<HTMLInputElement>){
         const {name, value} = event.target;
-        this.setState({ [name] : value } as Pick<AuthFormState, keyof AuthFormState>);
+        this.setState({ [name] : value, authorized: false } as Pick<AuthFormState, keyof AuthFormState>);
     }
 
     async handleSubmit(event: SyntheticEvent){
@@ -39,6 +41,12 @@ export class AuthForm extends React.Component<{}, AuthFormState>{
 
                 if(token){
                     localStorage.setItem('token', token as string);
+
+                    this.setState({
+                        login: this.state.login,
+                        password: this.state.password,
+                        authorized: true
+                    })
                 }
             }
         
@@ -52,13 +60,53 @@ export class AuthForm extends React.Component<{}, AuthFormState>{
     render(){
         return(
             <form onSubmit={this.handleSubmit}>
-                <label>Login</label>
-                <input type='text' value={this.state.login} name='login' onChange={this.handleChange} />
-                
-                <label>Password</label>
-                <input type='text' value={this.state.password} name='password' onChange={this.handleChange} />
+                <div className={styles["brutalist-container"]}>
+                    <input
+                        placeholder="..."
+                        className={styles["brutalist-input"]}
+                        type="text"
+                        value={this.state.login} 
+                        name="login"
+                        onChange={this.handleChange}
+                    />
+                    <label className={styles['brutalist-label']}>Login</label>
+                </div>
 
-                <input type='submit' value='Submit' />
+                <div className={styles["brutalist-container"]}>
+                    <input
+                        placeholder="..."
+                        className={styles["brutalist-input"]}
+                        type="text"
+                        value={this.state.password} // Здесь должна быть ваша логика состояния
+                        name="password"
+                        onChange={this.handleChange}
+                    />
+                    <label className={styles['brutalist-label']}>Password</label>
+                </div>
+                <div className={styles['brutalist-container']}>
+                    {
+                        this.state.authorized ? <span>Теперь ты <b>{this.state.login}</b> !</span> : <></>
+                    }
+
+                </div>
+                <input
+                    className={styles['brutalist-input-submit']}
+                    type='submit'
+                    value='Sign Up'
+                    onClick={this.handleSubmit}
+                />
+
+                <div className={styles['brutalist-container']}>
+                    <h2>Warning!</h2>
+                    <p>
+                        Есть только форма регистрации,
+                        если повторно зарегистрироваться, то данные[Likes] будут отображаться предудыщего пользователя до следующей перезагрузки сайта.
+                        Восстановить доступ к предыдущему пользователю невозможно.
+                        <br/>
+                        Проект находится на стадии Pre-[Not Soon]Post-Alpha-Development.
+                        
+                    </p>
+                </div>
             </form>
             
         )
--- front/src/components/Navbar/Navbar.tsx
+++ front/src/components/Navbar/Navbar.tsx
@@ -1,40 +1,59 @@
-import React from 'react'
+import React, { useState } from 'react';
+import { links } from './links.json';
+import { Link, Path, PathMatch, useMatch, useResolvedPath } from 'react-router-dom';
+import { AuthForm } from '../Auth/Auth';
+import Modal from '../Modal/Modal';
+import Button from '../Button/Button';
 import styles from './Navbar.module.css'
-import { links } from './links.json'
-import {Link, Path, PathMatch, useMatch, useResolvedPath} from 'react-router-dom'
-import { AuthForm } from '../Auth/Auth'
 
 type Link = {
-    label : string;
-    href : string;
+  label: string;
+  href: string;
 };
 
-const Links : React.FC<{links: Link[]}> = ({links} )=> {
-    return(
-        <div className={styles['nav-links']}>
-            {links.map((link : Link) => {
-                const resolvedPath: Path = useResolvedPath(link.href);
-                const isActive: PathMatch<string> | null = useMatch({path: resolvedPath.pathname, end:true});
+const Links: React.FC<{ links: Link[] }> = ({ links }) => {
+  const [isAuthFormOpened, setAuthFormOpened] = useState<boolean>(false);
 
-                return (
-                    <div key={link.href} className={styles['nav-link'] + ( isActive ? " " + styles['nav-link-active'] : "" )}>
-                        <Link to={link.href}>
-                            {link.label}
-                        </Link>
-                    </div>
-                )
-            })}
-        </div>
-    )
-}
+  const openModal = () => setAuthFormOpened(true);
+  const closeModal = () => setAuthFormOpened(false);
 
-const Navbar:  React.FC<{}> = () => {
-    return (
-        <nav className={styles.navbar}>
-            <Links links={ links } />
-            <AuthForm />
-        </nav>
-    );
-}
+  return (
+    <div className={styles['nav-links']}>
+      <div className={styles['nav-left']}>
+        {links.map((link: Link) => {
+          const resolvedPath: Path = useResolvedPath(link.href);
+          const isActive: PathMatch<string> | null = useMatch({ path: resolvedPath.pathname, end: true });
+
+          return (
+            <div
+              key={link.href}
+              className={`${styles['nav-link']} ${isActive ? styles['nav-link-active'] : ''}`}
+            >
+              <Link to={link.href}>
+                {link.label}
+              </Link>
+            </div>
+          );
+        })}
+      </div>
+      <div className={styles['nav-right']}>
+        <Button onClick={openModal}>
+            USER
+        </Button>
+      </div>
+      <Modal isOpen={isAuthFormOpened} onClose={closeModal}>
+        <AuthForm />
+      </Modal>
+    </div>
+  );
+};
+
+const Navbar: React.FC<{}> = () => {
+  return (
+    <nav className={styles.navbar}>
+      <Links links={links} />
+    </nav>
+  );
+};
 
 export default Navbar;
